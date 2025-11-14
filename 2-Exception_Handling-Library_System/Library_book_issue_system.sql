125370c1c834d2b2735eee5d1b3366ff6cb32478
Amaan-Tai <amaan.tai@tntra.io>
2025-11-14 10:55:29 +0530

    feat: add Books table and seed it with initial 20 books

---
+++ 2-Exception_Handling-Library_System/Library_book_issue_system.sql
@@ -0,0 +1,30 @@
+-- Create Books Table 
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    book_name VARCHAR2(50),
+    available_qty NUMBER
+);
+
+-- Insert data into table
+INSERT ALL
+    INTO Books (book_id, book_name, available_qty) VALUES (101, 'Ikigai', 5)
+    INTO Books (book_id, book_name, available_qty) VALUES (102, 'Atomic Habits', 7)
+    INTO Books (book_id, book_name, available_qty) VALUES (103, 'The Alchemist', 4)
+    INTO Books (book_id, book_name, available_qty) VALUES (104, 'Rich Dad Poor Dad', 6)
+    INTO Books (book_id, book_name, available_qty) VALUES (105, 'The Psychology of Money', 8)
+    INTO Books (book_id, book_name, available_qty) VALUES (106, 'Think and Grow Rich', 3)
+    INTO Books (book_id, book_name, available_qty) VALUES (107, 'The Power of Your Subconscious Mind', 5)
+    INTO Books (book_id, book_name, available_qty) VALUES (108, 'The 7 Habits of Highly Effective People', 2)
+    INTO Books (book_id, book_name, available_qty) VALUES (109, 'Sapiens: A Brief History of Humankind', 4)
+    INTO Books (book_id, book_name, available_qty) VALUES (110, 'The Monk Who Sold His Ferrari', 5)
+    INTO Books (book_id, book_name, available_qty) VALUES (111, 'Deep Work', 3)
+    INTO Books (book_id, book_name, available_qty) VALUES (112, 'Start With Why', 6)
+    INTO Books (book_id, book_name, available_qty) VALUES (113, 'The Subtle Art of Not Giving a F*ck', 7)
+    INTO Books (book_id, book_name, available_qty) VALUES (114, 'Can’t Hurt Me', 3)
+    INTO Books (book_id, book_name, available_qty) VALUES (115, 'The Power of Now', 5)
+    INTO Books (book_id, book_name, available_qty) VALUES (116, 'Man’s Search for Meaning', 4)
+    INTO Books (book_id, book_name, available_qty) VALUES (117, 'Meditations', 2)
+    INTO Books (book_id, book_name, available_qty) VALUES (118, 'Hooked: Habit-Forming Products', 6)
+    INTO Books (book_id, book_name, available_qty) VALUES (119, 'The Lean Startup', 3)
+    INTO Books (book_id, book_name, available_qty) VALUES (120, 'Zero to One', 8)
+SELECT * FROM dual;
